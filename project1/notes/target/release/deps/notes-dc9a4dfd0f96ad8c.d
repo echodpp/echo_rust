6b8d9a566ff17e75ecd3072b82b170fafd452164
Echo chen <90811429+echodpp@users.noreply.github.com>
2023-02-08 15:32:22 +0000

    233

---
+++ project1/notes/target/release/deps/notes-dc9a4dfd0f96ad8c.d
@@ -0,0 +1,5 @@
+/workspaces/echo_rust/project1/notes/target/release/deps/notes-dc9a4dfd0f96ad8c: src/main.rs
+
+/workspaces/echo_rust/project1/notes/target/release/deps/notes-dc9a4dfd0f96ad8c.d: src/main.rs
+
+src/main.rs:
---
+++ project1/notes/target/release/notes.d
@@ -0,0 +1,1 @@
+/workspaces/echo_rust/project1/notes/target/release/notes: /workspaces/echo_rust/project1/notes/src/main.rs
